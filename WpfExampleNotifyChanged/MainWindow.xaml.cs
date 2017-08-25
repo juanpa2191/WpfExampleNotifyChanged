10323f5417ab5f1563a18528573541f4eaef3188
juan pablo ospina <juanpa2191@hotmail.com>
2017-08-25 12:07:29 -0500

    Agregar archivos de proyecto.

---
+++ WpfExampleNotifyChanged/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfExampleNotifyChanged.ViewModel;
+
+namespace WpfExampleNotifyChanged
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel _main = new MainViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _main;
+        }
+
+        private void Red_Clicked(object sender, RoutedEventArgs e)
+        {
+            _main.SetBackground(Brushes.Red);
+        }
+
+        private void Blue_Clicked(object sender, RoutedEventArgs e)
+        {
+            _main.SetBackground(Brushes.Blue);
+        }
+
+        private void Yellow_Clicked(object sender, RoutedEventArgs e)
+        {
+            _main.SetBackground(Brushes.Yellow);
+        }
+    }
+}
---
+++ WpfExampleNotifyChanged/ViewModel/BackgroundViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using WpfExampleNotifyChanged.Model;
+
+namespace WpfExampleNotifyChanged.ViewModel
+{
+    public class BackgroundViewModel : ObservableObject
+    {
+        private Brush _color;
+
+        public Brush Color
+        {
+            get
+            {
+                if (_color == null)
+                    return Brushes.Orange;
+                return _color;
+            }
+            set
+            {
+                _color = value;
+                OnPropertyChanged("Color");
+
+            }
+        }
+
+    }
+}
